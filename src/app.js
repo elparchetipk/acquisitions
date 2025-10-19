7ec6813dc378c510bfecdb3f5d28cd3d1cd36c55
El Parche Dev <158541050+elparchetipk@users.noreply.github.com>
2025-10-18 22:24:14 -0500

    feat: login cofing added w/ helmet, morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim())}}));
+
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
 
